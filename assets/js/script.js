3deb7a440339849d3b7109be5dda0ca228897454
Krystyna Szurmańska <kszurmanska@gmail.com>
2018-07-15 23:32:48 +0200

    Update index.html, style.css and script.js to add Projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,31 @@
 var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
+var flkty = new Flickity(elem, {
   // options
   cellAlign: 'left',
   contain: true
+});
+
+
+var elem = document.querySelector('.grid');
+var iso = new Isotope(elem, {
+  itemSelector: '.grid-item',
+  layoutMode: 'fitRows'
+});
+
+var filtersElem = document.querySelector('.filters-button-group');
+filtersElem.addEventListener( 'click', function( event ) {
+  // only work with buttons
+  if ( !matchesSelector( event.target, 'button' ) ) {
+    return;
+  }
+  var filterValue = event.target.getAttribute('data-filter');
+  // use matching filter function
+  iso.arrange({ filter: filterValue });
+ 
+});
+
+var showMore = document.querySelector('.showMore');
+showMore.addEventListener( 'click', function( event ) {
+   iso.arrange({ filter: '*' });
+
 });
