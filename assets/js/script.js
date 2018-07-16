88e018aaced4fdf357c736f6edfe35119c553c86
Krystyna Szurmańska <kszurmanska@gmail.com>
2018-07-16 11:44:41 +0200

    Update index.html and style.css to add Team

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,10 @@ var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity(elem, {
   // options
   cellAlign: 'left',
-  contain: true
+  contain: true,
+  prevNextButtons: false,
+  setGallerySize: false,
+  draggable: true
 });
 
 
