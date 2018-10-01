aef3b43ed00211b95f154ed78c429ad252184f37
Krystyna Szurmańska <kszurmanska@gmail.com>
2018-10-01 11:55:05 +0200

    Update script.js

--- assets/js/script-old.js
+++ assets/js/script-old.js
@@ -53,7 +53,7 @@ var iso = new Isotope(grid, {
   itemSelector: '.grid-item',
   layoutMode: 'fitRows',
   masonry: {
-    isFitWidth: true
+      isFitWidth: true
   }
 });
 
@@ -78,82 +78,23 @@ showMore.addEventListener( 'click', function( event ) {
 
 });*/
 
-var gridButtons = document.querySelectorAll(".grid-button");
-console.log(gridButtons);
-
-for (var i = 0; i < gridButtons.length; i++) {
-  console.log(gridButtons[i].outerText);
-  gridButtons[i].addEventListener("click", function (event) {
-    //  var activeButton = document.activeElement.id;
-    //  console.log(activeButton);
-    changeActiveButton(document.activeElement.id);
-    var showMoreButton = document.getElementById('show-more-button');
-    showMoreButton.style.display="inline";
-  });
-};
-
-var activeButtonId = "all-projects";
-console.log(activeButtonId);
-
-function changeActiveButton(idOfActiveButton) {
-  activeButtonId = idOfActiveButton;
-  console.log(activeButtonId)
-}
-
 var showMore = document.querySelector('.showMore');
-/*
 showMore.addEventListener("click", function(event) {
-
-  var focused = document.querySelector(":active");
-    console.log(focused);
-});*/
-
-showMore.addEventListener("click", function (event) {
-
-  console.log(activeButtonId);
-  var url = "./assets/json/" + activeButtonId + ".json";
+  var url = "./assets/json/photos.json";
 
   var xhr = new XMLHttpRequest();
   xhr.open("GET", url);
-  xhr.addEventListener("load", function () {
+  xhr.addEventListener("load", function() {
     var response = JSON.parse(xhr.response);
 
     console.log(response);
-    loadTemplate(response);
   });
   xhr.send();
-
-  /*var template = document.getElementById('template').innerHTML;
-  var rendered = Mustache.render(template);
-  document.getElementById('mustache').innerHTML = rendered;
-  console.log(grid);
-  var z = document.createElement('div'); 
-  z.innerHTML =   ' <div class="grid-item misc photos"><div class="layer">  <p>ABOUT</p>  <p> Praesent metus urna, feugiat a placerat elementum at leo</p></div><img src="assets/images/img12.png" alt=""></div>';
-  grid.appendChild( z);
-  iso.appended( z );
-  iso.layout();*/
 });
-/*
-iso.appendChild(response);
-iso.appended(response);*/
 
-function loadTemplate(response) {
-  console.log(response);
-  console.log(response.data[0].content);
-  console.log(response.data[0].imageUrl);
-  console.log(response.data[0].class);
-  for (var i = 0; i < response.data.length; i++) {
-    var z = "";
-    z = document.createElement('div');
-    z.innerHTML = ' <div class="grid-item ' + response.data[i].class + '"><div class="layer">  <p>ABOUT</p>  <p>' + response.data[i].content + '</p></div><img src="' + response.data[i].imageUrl + '" alt=""></div>';
-    grid.appendChild(z);
-    iso.appended(z);
-    iso.layout();
-    
-  }
-  var showMoreButton = document.getElementById('show-more-button');
-    showMoreButton.style.display="none";
-}
+//iso.appendChild(response);
+//iso.appended(response);
+
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
